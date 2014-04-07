e1248145a27e286640471f6e50515458849bfd5f
jet-black <ovechkin.dm@gmail.com>
2014-04-08 00:20:43 +0400

    fix Future Utils

--- src/main/java/me/jetblack/examples/client/util/FutureUtils.java
+++ src/main/java/me/jetblack/examples/client/util/FutureUtils.java
@@ -2,25 +2,34 @@ package me.jetblack.examples.client.util;
 
 import org.fusesource.restygwt.client.future.Future;
 import org.fusesource.restygwt.client.future.FutureImpl;
+import org.fusesource.restygwt.client.future.util.Try;
+
+import java.util.function.Consumer;
 
 public class FutureUtils {
 
     public static <E> Future<ExtendedList<E>> toFutureOfList(ExtendedList<Future<E>> source) {
         FutureImpl<ExtendedList<E>> init = new FutureImpl<>();
         init.completeWithSuccess(new ExtendedArrayList<E>());
-        return source.foldLeft(init, (Future<ExtendedList<E>> prev, Future<E> current) -> {
+        return source.foldLeft(init, (prev, current) -> {
             FutureImpl<ExtendedList<E>> next = new FutureImpl<>();
-            current.onComplete(item -> {
-                prev.onComplete(items -> {
-                    if (!item.isSuccess() || !items.isSuccess()) {
-                        next.completeWithFailure(new RuntimeException());
-                    } else {
-                        ExtendedList<E> result = new ExtendedArrayList<E>();
-                        result.addAll(items.get());
-                        result.add(item.get());
-                        next.completeWithSuccess(result);
-                    }
-                });
+            current.onComplete(new Consumer<Try<E>>() {
+                @Override
+                public void accept(Try<E> item) {
+                    prev.onComplete(new Consumer<Try<ExtendedList<E>>>() {
+                        @Override
+                        public void accept(Try<ExtendedList<E>> items) {
+                            if (!item.isSuccess() || !items.isSuccess()) {
+                                next.completeWithFailure(new IllegalArgumentException());
+                            } else {
+                                ExtendedList<E> result = new ExtendedArrayList<E>();
+                                result.addAll(items.get());
+                                result.add(item.get());
+                                next.completeWithSuccess(result);
+                            }
+                        }
+                    });
+                }
             });
             return next;
         });
@@ -28,7 +37,17 @@ public class FutureUtils {
 
     public static <T> Future<T> flatten(Future<Future<T>> source) {
         FutureImpl<T> result = new FutureImpl<>();
-        source.forEach(f -> f.onComplete(r -> result.completeWithResult(r)));
+        source.forEach(new Consumer<Future<T>>() {
+            @Override
+            public void accept(Future<T> f) {
+                f.onComplete(new Consumer<Try<T>>() {
+                    @Override
+                    public void accept(Try<T> t) {
+                        result.completeWithResult(t);
+                    }
+                });
+            }
+        });
         return result;
     }
 }
